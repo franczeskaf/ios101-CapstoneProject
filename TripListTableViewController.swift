1ea760509cff37156eb075623fcd2c1ef7f3e3c9
Franczeska Ferariza <156466809+franczeskaf@users.noreply.github.com>
2025-04-16 03:03:55 -0400

    Update TripListTableViewController.swift

--- TripListTableViewController.swift
+++ TripListTableViewController.swift
@@ -1,89 +1,95 @@
-//
-//  TripListTableViewController.swift
-//  StyleCast
-//
-//  Created by Franczeska Ferariza on 4/8/25.
-//
-
 import UIKit
 
 class TripListTableViewController: UITableViewController {
 
+    var trips: [Trip] = []
+
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        self.title = "My Trips"
     }
 
-    // MARK: - Table view data source
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        loadTrips()
+        sortTrips()
+        tableView.reloadData()
+    }
+
+    func loadTrips() {
+        if let data = UserDefaults.standard.data(forKey: "savedTrips") {
+            let decoder = JSONDecoder()
+            trips = (try? decoder.decode([Trip].self, from: data)) ?? []
+        }
+    }
+
+    func saveTrips() {
+        if let encoded = try? JSONEncoder().encode(trips) {
+            UserDefaults.standard.set(encoded, forKey: "savedTrips")
+        }
+    }
+
+    func sortTrips() {
+        // Show pinned trips first
+        trips.sort { $0.isPinned && !$1.isPinned }
+    }
+
+    // MARK: - Table View Data Source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        return trips.count
     }
 
-    /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TripCell", for: indexPath)
+        let trip = trips[indexPath.row]
+        let pinSymbol = trip.isPinned ? "📍 " : ""
+        cell.textLabel?.text = "\(pinSymbol)\(trip.city)"
         return cell
     }
-    */
 
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+    // MARK: - Swipe Actions (Delete + Pin)
+
+    override func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+
+        // Delete
+        let deleteAction = UIContextualAction(style: .destructive, title: "Delete") { [weak self] _, _, completionHandler in
+            self?.trips.remove(at: indexPath.row)
+            self?.saveTrips()
+            tableView.deleteRows(at: [indexPath], with: .automatic)
+            completionHandler(true)
+        }
+
+        // Pin / Unpin
+        let trip = trips[indexPath.row]
+        let pinTitle = trip.isPinned ? "Unpin" : "Pin"
+        let pinAction = UIContextualAction(style: .normal, title: pinTitle) { [weak self] _, _, completionHandler in
+            self?.trips[indexPath.row].isPinned.toggle()
+            self?.sortTrips()
+            self?.saveTrips()
+            tableView.reloadData()
+            completionHandler(true)
+        }
+
+        pinAction.backgroundColor = .systemBlue
+
+        let config = UISwipeActionsConfiguration(actions: [deleteAction, pinAction])
+        return config
     }
-    */
 
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
+    // MARK: - Navigation to Detail
 
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        if segue.identifier == "TripDetailSegue",
+           let destinationVC = segue.destination as? TripDetailViewController,
+           let indexPath = tableView.indexPathForSelectedRow {
+            destinationVC.selectedTrip = trips[indexPath.row]
+        }
     }
-    */
-
 }
+
+
